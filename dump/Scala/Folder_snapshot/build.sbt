eda21f8c107bbe1063c07d539f65d729b94774cb
Anurag Choudhary <anurag.choudhary@siemens.com>
2019-11-26 15:57:55 +0530

    deleting dump

--- dump/Scala/Folder_snapshot/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "ingestion"  
-  version := "1.0"  
-  scalaVersion := "2.11.8"  
-  autoScalaLibrary := false 
-scalaHome := Some(file("/home/rajeebp/scala/"))
--- dump/Scala/hive_internal_load_grp6.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export SPARK_MAJOR_VERSION=2
-spark-submit --class Ingestion --master yarn  --num-executors 2 --driver-memory 3g  --executor-memory 8g --executor-cores 4 --conf spark.scheduler.mode=FAIR  --conf spark.driver.cores=1  --conf spark.serializer=org.apache.spark.serializer.KryoSerializer  /home/rajeebp/FinalCode/target/scala-2.11/simple-project_2.11-1.0.jar filter=group_number=6 truncate=true
-echo "hive load group 6 loaded successfully"
